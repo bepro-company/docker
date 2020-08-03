4f178752214c5b1835e59a35974a3d9d70670224
namongk <namongk@gmail.com>
2020-08-03 14:40:29 +0900

    Modify Dockerfile

--- python_with_ffmpeg/build.sh
+++ python_with_ffmpeg/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t bepro/python_with_ffmpeg .
+docker build -t "python_with_ffmpeg:3.7" -t "python_with_ffmpeg" .
+docker tag python_with_ffmpeg:latest bepro/python_with_ffmpeg:latest
+docker tag python_with_ffmpeg:3.7 bepro/python_with_ffmpeg:3.7
 docker push bepro/python_with_ffmpeg:latest
+docker push bepro/python_with_ffmpeg:3.7
