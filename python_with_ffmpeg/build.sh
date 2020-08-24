f6a5d568aaece1cd3b71b6276501dfa5ea661801
Hanter Jung <hanter@bepro11.com>
2020-08-24 16:47:31 +0900

    Revert "Add libglib"
    This reverts commit fbc6ae94ad65d1ab91b3ec8e6b545006dcf17e88.

--- python_with_ffmpeg/build.sh
+++ python_with_ffmpeg/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker build -t "python_with_ffmpeg:3.7" -t "python_with_ffmpeg" .
-docker tag python_with_ffmpeg:3.7-test bepro/python_with_ffmpeg:3.7-test
+docker tag python_with_ffmpeg:latest bepro/python_with_ffmpeg:latest
+docker tag python_with_ffmpeg:3.7 bepro/python_with_ffmpeg:3.7
 docker push bepro/python_with_ffmpeg:latest
 docker push bepro/python_with_ffmpeg:3.7
--- python_with_ffmpeg/build_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-docker build -t "python_with_ffmpeg:test" .
-docker tag python_with_ffmpeg:test bepro/python_with_ffmpeg:test
-docker push bepro/python_with_ffmpeg:test
