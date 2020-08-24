fbc6ae94ad65d1ab91b3ec8e6b545006dcf17e88
Hanter Jung <hanter@bepro11.com>
2020-08-24 16:47:15 +0900

    Add libglib

--- python_with_ffmpeg/build.sh
+++ python_with_ffmpeg/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker build -t "python_with_ffmpeg:3.7" -t "python_with_ffmpeg" .
-docker tag python_with_ffmpeg:latest bepro/python_with_ffmpeg:latest
-docker tag python_with_ffmpeg:3.7 bepro/python_with_ffmpeg:3.7
+docker tag python_with_ffmpeg:3.7-test bepro/python_with_ffmpeg:3.7-test
 docker push bepro/python_with_ffmpeg:latest
 docker push bepro/python_with_ffmpeg:3.7
