559d6f4ff5fd089ab4181f8602299503b12695ba
seba <seba@bepro11.com>
2020-10-14 15:43:42 +0200

    Push to github container registry

--- lambda_python3.7/build.sh
+++ lambda_python3.7/build.sh
@@ -2,3 +2,13 @@
 
 docker build -t bepro/lambda_python3.7 .
 docker push bepro/lambda_python3.7:latest
+
+# https://docs.github.com/en/free-pro-team@latest/packages/managing-container-images-with-github-container-registry/pushing-and-pulling-docker-images#authenticating-to-github-container-registry
+if [[ $CR_USER ]] && [[ $CR_PAT ]]
+then
+  docker tag bepro/lambda_python3.7:latest ghcr.io/bepro-company/docker/lambda_python3.7:latest
+  echo $CR_PAT | docker login ghcr.io -u $CR_USER --password-stdin
+  docker push ghcr.io/bepro-company/docker/lambda_python3.7:latest
+else
+  echo "CR_USER, CR_PAT environment variables required!"
+fi
