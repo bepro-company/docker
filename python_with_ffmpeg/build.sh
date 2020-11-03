f179aa362e69dd2bf6ff3e8676b545f7b98fb1fb
seba <seba@bepro11.com>
2020-11-03 19:18:32 +0100

    Add Dockerfile for libcairo

--- python_with_ffmpeg/build.sh
+++ python_with_ffmpeg/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# default: for bepro-python-api
 docker build -t "python_with_ffmpeg:3.7" -t "python_with_ffmpeg" .
 docker tag python_with_ffmpeg:latest bepro/python_with_ffmpeg:latest
 docker tag python_with_ffmpeg:3.7 bepro/python_with_ffmpeg:3.7
+
 docker push bepro/python_with_ffmpeg:latest
 docker push bepro/python_with_ffmpeg:3.7
+
+# libcairo: for bepro-python github actions
+docker build -t "ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7-libcairo" -f Dockerfile.libcairo .
 
 # CR_USER: github user id
 # CR_PAT: https://docs.github.com/en/free-pro-team@latest/packages/managing-container-images-with-github-container-registry/pushing-and-pulling-docker-images#authenticating-to-github-container-registry
@@ -13,6 +18,10 @@ then
   docker tag bepro/python_with_ffmpeg:3.7 ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7
   echo $CR_PAT | docker login ghcr.io -u $CR_USER --password-stdin
   docker push ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7
+
+  # libcairo
+  docker push ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7-libcairo
 else
   echo "CR_USER, CR_PAT environment variables required!"
 fi
+
