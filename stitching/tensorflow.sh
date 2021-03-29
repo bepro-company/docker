09fc9692f66ce5a7bc774f31cc2e722fb463050a
sebatyler <sebatyler@gmail.com>
2021-03-29 12:24:18 +0900

    Add tensorflow installation

---
+++ stitching/tensorflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install bazel
+cd ~
+installer=bazel-0.26.1-installer-linux-x86_64.sh
+wget -O $installer https://github.com/bazelbuild/bazel/releases/download/0.26.1/$installer
+chmod +x $installer
+./$installer
+source /usr/local/lib/bazel/bin/bazel-complete.bash
+rm -fv $installer
+
+# install tensorflow cpp api
+cd ~
+git clone -b v2.2.0 https://github.com/FloopCZ/tensorflow_cc.git
+cd ~/tensorflow_cc/tensorflow_cc
+echo "2.0.0" > PROJECT_VERSION
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+make install
+
+pip3 install tensorflow-gpu==2.0.0a0
