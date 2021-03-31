3b84febea0664f3090cd77d145abb8e4bacc1671
sebatyler <sebatyler@gmail.com>
2021-03-31 11:04:23 +0900

    Fix checking cudnn version

--- stitching/tensorflow.sh
+++ stitching/tensorflow.sh
@@ -5,9 +5,8 @@ cd ~
 installer=bazel-0.26.1-installer-linux-x86_64.sh
 wget -O $installer https://github.com/bazelbuild/bazel/releases/download/0.26.1/$installer
 chmod +x $installer
-./$installer
+./$installer && rm -fv $installer
 source /usr/local/lib/bazel/bin/bazel-complete.bash
-rm -fv $installer
 
 # install tensorflow cpp api
 cd ~
@@ -16,5 +15,6 @@ cd ~/tensorflow_cc/tensorflow_cc
 echo "2.0.0" > PROJECT_VERSION
 mkdir build
 cd build
+sed -i 's/cudnn\.h/cudnn_version.h/g' tensorflow/third_party/gpus/find_cuda_config.py
 cmake ..
 make -j$(nproc) && make install
