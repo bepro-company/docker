5de58fc03622ccebca5b51a181963ac70afc9788
sebatyler <sebatyler@gmail.com>
2021-03-31 14:41:06 +0900

    Change version

--- stitching/tensorflow.sh
+++ stitching/tensorflow.sh
@@ -8,6 +8,9 @@ chmod +x $installer
 ./$installer && rm -fv $installer
 source /usr/local/lib/bazel/bin/bazel-complete.bash
 
+# cd /usr/local/cuda/lib64
+# ln -s libcusolver.so.10 libcusolver.so.11
+
 # install tensorflow cpp api
 cd ~
 git clone -b v2.2.0 https://github.com/FloopCZ/tensorflow_cc.git
@@ -15,6 +18,6 @@ cd ~/tensorflow_cc/tensorflow_cc
 echo "2.0.0" > PROJECT_VERSION
 mkdir build
 cd build
-sed -i 's/cudnn\.h/cudnn_version.h/g' tensorflow/third_party/gpus/find_cuda_config.py
 cmake ..
+sed -i 's/cudnn\.h/cudnn_version.h/g' tensorflow/third_party/gpus/find_cuda_config.py
 make -j$(nproc) && make install
