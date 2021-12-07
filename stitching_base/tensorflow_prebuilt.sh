e976d4d30278ededb27f9062b9eb621d649482e7
Jahwan Oh <jahwan.oh@bepro11.com>
2021-12-07 11:39:13 +0000

    merge lines

--- stitching_base/tensorflow_prebuilt.sh
+++ stitching_base/tensorflow_prebuilt.sh
@@ -8,7 +8,7 @@ cmake ..
 make -j8
 sudo make install
 cd ~
-rm protobuf-cpp-3.9.2.zip
+rm -v protobuf-cpp-3.9.2.zip
 
 #tensorflow cc
 cd ~
@@ -16,16 +16,13 @@ wget https://storage.googleapis.com/bepro-dev/scouting_feed/tensorflow/tensorflo
 mkdir tensorflow_cc
 cd tensorflow_cc
 unzip ../tensorflow_cpp_jetson_2.3.1.zip
-cp -r include/* /usr/local/include
+cp -rv include/* /usr/local/include
 
-rm libtensorflow_cc.so
-rm libtensorflow_cc.so.2
+rm -v libtensorflow_cc.so libtensorflow_cc.so.2 libtensorflow_framework.so.2
 ln -s libtensorflow_cc.so.2.3.1 libtensorflow_cc.so
 ln -s libtensorflow_cc.so.2.3.1 libtensorflow_cc.so.2
-rm libtensorflow_framework.so.2
 ln -s libtensorflow_framework.so.2.3.1 libtensorflow_framework.so.2
 
-cp libtensorflow_* /usr/local/lib
+cp -v libtensorflow_* /usr/local/lib
 cd ~
-rm -r tensorflow_cc
-rm tensorflow_cpp_jetson_2.3.1.zip
+rm -rfv tensorflow_cc tensorflow_cpp_jetson_2.3.1.zip
