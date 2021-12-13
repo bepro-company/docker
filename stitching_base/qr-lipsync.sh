34183f6a0b1332d0d585a249bbd66fb1ad82cfb5
Furkan Elibol <felibol@bepro11.com>
2021-12-13 16:58:23 +0100

    python path changed for qr-lipsync tool

--- stitching_base/qr-lipsync.sh
+++ stitching_base/qr-lipsync.sh
@@ -5,8 +5,4 @@ set -e
 git clone https://github.com/UbiCastTeam/qr-lipsync.git
 cd qr-lipsync
 
-# default python 3
-python3 setup.py install
-
-# python 3.6
-which python3.6 && python3.6 setup.py install
+/usr/bin/python3.6 setup.py install
