e2e6a7f37dcfbd537a0c9f829d5a60700dfd6ea4
jahwan oh <jahwan.oh@bepro11.com>
2021-12-23 14:14:54 +0100

    add ffmpeg and add python3.6 setuptool

--- stitching_base/qr-lipsync.sh
+++ stitching_base/qr-lipsync.sh
@@ -7,11 +7,4 @@ cd qr-lipsync
 
 # default python 3
 python3 setup.py install
-
-# python 3.6 (adding setuptools for arm64's python3.6)
-if which python3.6
-then
-    python3.6 -m pip install setuptools
-    python3.6 setup.py install
-fi
 which python3.6 && python3.6 setup.py install
