3b1a7e66ec1f9ba230e35e492ee1142a07a76b05
jahwan oh <jahwan.oh@bepro11.com>
2022-03-07 14:24:23 +0000

    chang opencv and tensorflow version

--- stitching_base/build.sh
+++ stitching_base/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-name_tag="stitching_base:21.13"
+name_tag="stitching_base:21.14"
 
 docker build -t bepro/$name_tag .
 
--- stitching_base/opencv.sh
+++ stitching_base/opencv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # opencv 3.4.12
-wget -qO opencv.zip https://github.com/opencv/opencv/archive/3.4.12.zip
-wget -qO opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.12.zip
+wget -qO opencv.zip https://github.com/opencv/opencv/archive/3.4.16.zip
+wget -qO opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.16.zip
 unzip opencv.zip
 unzip opencv_contrib.zip
 rm -fv opencv*.zip
 
-cd opencv-3.4.12
+cd opencv-3.4.16
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
@@ -21,7 +21,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D ENABLE_FAST_MATH=1 \
   -D CUDA_FAST_MATH=1 \
   -D OPENCV_ENABLE_NONFREE=True \
-  -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.12/modules \
+  -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.16/modules \
   -D CUDA_NVCC_FLAGS=--expt-relaxed-constexpr \
   -D BUILD_EXAMPLES=OFF \
   -D BUILD_NEW_PYTHON_SUPPORT=ON \
