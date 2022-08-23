2d7da4a0ea642fabf1d095df38d504968c63c194
junho.cha <junho.cha@bepro11.com>
2022-08-23 11:11:15 +0000

    update build script version

--- python_with_ffmpeg/build.sh
+++ python_with_ffmpeg/build.sh
@@ -2,18 +2,18 @@
 
 docker build -t "python_with_ffmpeg:3.8" .
 docker tag python_with_ffmpeg:latest bepro/python_with_ffmpeg:latest
-docker tag python_with_ffmpeg:3.7 bepro/python_with_ffmpeg:3.7
+docker tag python_with_ffmpeg:3.8 bepro/python_with_ffmpeg:3.8
 
 docker push bepro/python_with_ffmpeg:latest
-docker push bepro/python_with_ffmpeg:3.7
+docker push bepro/python_with_ffmpeg:3.8
 
 # CR_USER: github user id
 # CR_PAT: https://docs.github.com/en/free-pro-team@latest/packages/managing-container-images-with-github-container-registry/pushing-and-pulling-docker-images#authenticating-to-github-container-registry
 if [[ $CR_USER ]] && [[ $CR_PAT ]]
 then
-  docker tag bepro/python_with_ffmpeg:3.7 ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7
+  docker tag bepro/python_with_ffmpeg:3.8 ghcr.io/bepro-company/docker/python_with_ffmpeg:3.8
   echo $CR_PAT | docker login ghcr.io -u $CR_USER --password-stdin
-  docker push ghcr.io/bepro-company/docker/python_with_ffmpeg:3.7
+  docker push ghcr.io/bepro-company/docker/python_with_ffmpeg:3.8
 else
   echo "CR_USER, CR_PAT environment variables required!"
 fi
