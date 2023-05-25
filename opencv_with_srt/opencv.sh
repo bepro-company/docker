48bef11e06aa82cbc010f07bd79c79e03b95b2f4
jahwan oh <jahwan.oh@bepro11.com>
2023-05-25 17:08:57 +0100

    testing opencv-srt

--- opencv_with_srt/opencv.sh
+++ opencv_with_srt/opencv.sh
@@ -22,11 +22,12 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.16/modules \
   -D CUDA_NVCC_FLAGS=--expt-relaxed-constexpr \
   -D BUILD_EXAMPLES=OFF \
-  -D BUILD_NEW_PYTHON_SUPPORT=ON \
-  -D BUILD_opencv_python3=ON \
-  -D HAVE_opencv_python3=ON \
-  -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) \
-  -D BUILD_opencv_cudacodec=OFF \
+  -D PYTHON3_EXECUTABLE=$(which python3) \
+  -D PYTHON_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+  -D PYTHON_INCLUDE_DIR2=$(python3 -c "from os.path import dirname; from distutils.sysconfig import get_config_h_filename; print(dirname(get_config_h_filename()))") \
+  -D PYTHON_LIBRARY=$(python3 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))") \
+  -D PYTHON3_NUMPY_INCLUDE_DIRS=$(python3 -c "import numpy; print(numpy.get_include())") \
+  -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")   -D BUILD_opencv_cudacodec=OFF \
   -D WITH_V4L=OFF \
   -D WITH_LIBV4L=ON \
   ../
