c1c53d9050d603fb1b3d1d83d5d34c28b56e7e75
jahwan oh <jahwan.oh@bepro11.com>
2023-05-30 17:00:18 +0100

    testing cv2

--- opencv_with_srt/opencv-python.sh
+++ opencv_with_srt/opencv-python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone --recursive https://github.com/opencv/opencv-python.git
-cd ./opencv-python && git checkout -b 3.4 origin/3.4
+cd ./opencv-python && git checkout -b 3.4 origin/3.4 && cp /root/pyproject.toml .
 export CMAKE_ARGS="-DPYTHON3_LIMITED_API=ON -DWITH_FFMPEG=ON"
 pip wheel . --verbose
