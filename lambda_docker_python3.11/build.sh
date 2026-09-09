42434f59a662e783a3621d5a1638f2e291570634
Hanter Jung <hanter@bepro11.com>
2026-09-09 12:51:57 +0900

    add platform

--- lambda_docker_python3.11/build.sh
+++ lambda_docker_python3.11/build.sh
@@ -2,7 +2,7 @@
 
 name_tag="lambda_docker_python:3.11"
 
-docker build -t bepro/$name_tag .
+docker build --platform linux/amd64 -t bepro/$name_tag .
 
 # CR_USER: github user id
 # CR_PAT: https://docs.github.com/en/free-pro-team@latest/packages/managing-container-images-with-github-container-registry/pushing-and-pulling-docker-images#authenticating-to-github-container-registry
